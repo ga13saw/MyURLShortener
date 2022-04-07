9223c85539dc6720a2c76928049870c24aa2a425
Ganesh <sawganeshkumar15@gmail.com>
2022-04-07 18:47:19 +0530

    First version(v1.0) of My URL Shortener application

---
+++ src/test/java/com/code4you/MyUrlShortener/MyUrlShortenerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.code4you.MyUrlShortener;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyUrlShortenerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
